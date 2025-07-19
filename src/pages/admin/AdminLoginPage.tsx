47fedb24baf913c95e71d326e066534cc3739c59
Sanjib Dolai <90773927+sanjibdolai@users.noreply.github.com>
2025-07-19 22:15:54 +0530

    updated

--- src/pages/admin/AdminLoginPage.tsx
+++ src/pages/admin/AdminLoginPage.tsx
@@ -44,7 +44,7 @@ function AdminLoginPage() {
         <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
                 <img
-                    className="mx-auto h-12 w-auto"
+                    className="mx-auto h-32 w-auto"
                     src={logo}
                     alt="Restaurant Logo"
                 />
